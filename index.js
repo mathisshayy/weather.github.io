22ee144adae11095a21a24fae13c68d3d054fbfb
Shay Mathis <70081339+mathisshayy@users.noreply.github.com>
2020-10-30 00:56:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-$.getJSON(
-    "http://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/{x}/{y}&appid={&appid=1988f65fbc72677f9083449ebc3c953e}", 
-    function(data){
-    console.log(data);
+$(document).ready(function () {
+
+    var lat;
+    var long;
+
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+
+            var api = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + long + '';
+
+            $.getJSON(api, function (res) {
+
+                var celsius = res.main.temp;
+                var farenheit = (celsius * 1.8) + 32;
+
+                var location = res.name;
+
+
+                $('.location').html(location);
+                $('.temp').html(Math.floor(farenheit));
+                $('.description').html(res.weather[0].description);
+                $('.type').attr('id', res.weather[0].main);
+                $('.Get Weather').on('click', function () {
+                    if ($('.temp').html() == (Math.floor(farenheit))) {
+                        $('.temp-type').html('°F');
+
+                    } else {
+                        $('.temp').html(Math.floor(celsius));
+                        $('.temp-type').html('°C');
+                    }
+                });
+
+            });
+        });
+    }
 });
-
-var temp = data.weather.temp;
-var weather = data.weather[0].main;
-
-$('.weather').append(weather);
-$(".temp").append();
